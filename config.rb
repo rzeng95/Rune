8fb5ddfb036a7f5c812576d16a0461f9f43cccda
Brandon Ly <brandonly@live.com>
2016-05-03 04:16:05 -0700

    Trimmed rune armor and made navbar even better. * Also fixed kanban board page a bit.

--- config.rb
+++ config.rb
@@ -9,10 +9,10 @@ css_dir = "public/stylesheets"
 sass_dir = "sass"
 images_dir = "public/images"
 javascripts_dir = "public/javascripts"
-fonts_dir = "public/fonts"
+fonts_dir = "public/fonts/"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed # :expanded or :nested or :compact or :compressed
+output_style = :compact # :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
