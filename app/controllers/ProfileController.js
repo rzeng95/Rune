326844cda6aaffd5053f11b8463abae05d83f329
Roland <roland.zeng@gmail.com>
2016-05-04 13:15:59 -0700

    users can now be added to selected projects

--- app/controllers/ProfileController.js
+++ app/controllers/ProfileController.js
@@ -4,8 +4,11 @@
 */
 
 var User = require('../models/user.js');
+var Project = require('../models/project.js');
 
 var Helper = require('../models/helpers.js');
+
+var async = require('async');
 
 module.exports = function(app, passport) {
 
@@ -45,7 +48,93 @@ module.exports = function(app, passport) {
             }
         });
 
-    }) // End of app.get('/u/:userid')
+    }); // End of app.get('/u/:userid')
+
+    app.get('/u/:userid/adduser/:projectid', Helper.isLoggedIn, Helper.doesUserExist, Helper.doesProjectExist, function(req,res) {
+        // Five things need to happen:
+        // First, find the user given by req.params.userid
+        // Then, find the project given by req.params.projectid
+        // Make sure the user isn't already a member of the project
+        // Add the user to that project. Save
+        // Add the project to the user's list of projects. Save
+
+        async.waterfall([
+
+            function findUser(callback) {
+                User.findOne({'local.userid' : req.params.userid}, function(err,foundUser) {
+                    if (err) {
+                        callback(err);
+                    } else {
+                        callback(null, foundUser); // the found user gets passed into foundUser variable
+                    }
+                });
+            },
+            function findProject(foundUser, callback) {
+                Project.findOne({'projectid': req.params.projectid}, function(err,foundProject) {
+                    if (err) {
+                        callback(err);
+                    } else {
+                        callback(null, foundUser, foundProject); // now both the found user and the found project can be modified in the next function
+                    }
+                })
+
+            },
+            function checkIfUserIsAlreadyMember(foundUser, foundProject, callback) {
+                var membersList = foundProject.members;
+                console.log(membersList);
+                if (membersList.indexOf(foundUser.local.email) === -1) {
+                    callback(null, foundUser, foundProject);
+                } else {
+                    callback(1);
+                }
+
+            },
+            function addUserToProject(foundUser, foundProject, callback) {
+                var userEmail = foundUser.local.email;
+                foundProject.members.push(userEmail);
+                foundProject.save(function(err) {
+                    if (err) {
+                        callback(err);
+                    } else {
+                        callback(null, foundUser, foundProject);
+                    }
+                });
+
+            },
+            function addProjectToUser(foundUser, foundProject, callback) {
+                foundUser.local.projects.push({
+                    projectkey : foundProject.projectkey,
+                    projectid : (foundProject.projectid).toString()
+                });
+                foundUser.save(function(err) {
+                    if (err) {
+                        callback(err);
+                    } else {
+                        callback(null);
+                    }
+
+                });
+            }
+
+        ], function(err){
+            if (err) {
+                if (err == 1) {
+                    console.log("user already a member of project");
+                    //ToDo: error popup modal
+                    res.redirect('/p/' + req.params.projectid + '/');
+                } else {
+                    throw err;
+                }
+            } else {
+                res.redirect('/p/' + req.params.projectid + '/');
+            }
+
+        });
+
+    });
+
+
+
 
     //generates a page with all users, and a link to their profiles
     app.get('/users', Helper.isLoggedIn, function(req,res) {
@@ -56,10 +145,14 @@ module.exports = function(app, passport) {
             } else {
                 var projectList = req.user.local.projects;
                 res.render('users.jade', {
-                    user : req.user,
-                    userlist : users,
+                    // These are navbar variables
                     loggedIn : req.isAuthenticated(),
-                    firstname: req.user.local.firstname
+                    projList : req.user.local.projects,
+                    firstname : req.user.local.firstname,
+
+
+                    user : req.user,
+                    userlist : users
                 });
             }
         });
