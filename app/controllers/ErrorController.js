f50ddef7817230c93bbbbebb2e48b1b4941e0a46
Roland <roland.zeng@gmail.com>
2016-05-15 15:28:46 -0700

    BUGFIX: error pages were crashing if user was not logged in

--- app/controllers/ErrorController.js
+++ app/controllers/ErrorController.js
@@ -6,11 +6,20 @@ module.exports = function(app, passport) {
 
     // Handle 404
     app.use(function(req, res) {
+        var loggedIn;
+        var projList;
+        var firstName;
+
+        if (req.isAuthenticated()) {
+            loggedIn = 1;
+            projList = req.user.local.projects;
+            firstName = req.user.local.firstname;
+        }
         res.status(404).render('error.jade', {
             // These variables are required for the navbar
-            loggedIn : req.isAuthenticated(),
-            projList : req.user.local.projects,
-            firstname : req.user.local.firstname,
+            loggedIn : loggedIn,
+            projList : projList,
+            firstname : firstName,
 
             errorMessage : '404: Not Found'
         });
@@ -18,11 +27,20 @@ module.exports = function(app, passport) {
 
     // Handle 500
     app.use(function(error, req, res, next) {
+        var loggedIn;
+        var projList;
+        var firstName;
+
+        if (req.isAuthenticated()) {
+            loggedIn = 1;
+            projList = req.user.local.projects;
+            firstName = req.user.local.firstname;
+        }
         res.status(500).render('error.jade', {
             // These variables are required for the navbar
-            loggedIn : req.isAuthenticated(),
-            projList : req.user.local.projects,
-            firstname : req.user.local.firstname,
+            loggedIn : loggedIn,
+            projList : projList,
+            firstname : firstName,
 
             errorMessage :'500: Internal Server Error'
         });
