f4a0a8f88a4c0afd6004769905ae6182085ae6c9
Roland <roland.zeng@gmail.com>
2016-05-17 05:30:11 -0700

    updated test set

--- test/2--AuthenticationTests.js
+++ test/2--AuthenticationTests.js
@@ -6,7 +6,14 @@ var request = require('supertest');
 
 var app = require('../app');
 
+var User = require('../app/models/user.js');
+
 describe('Passport authentication', function() {
+
+    before(function(done) {
+        User.collection.drop();
+        done();
+    });
 
     it('Unsuccessful logins redirect back to /login', function(done){
         request(app)
@@ -20,6 +27,17 @@ describe('Passport authentication', function() {
 
     });
 
+    it('Signups should redirect to /profile', function(done) {
+        request(app)
+        .post('/signup')
+        .type('form')
+        .send({firstname: 'roland', lastname: 'zeng', email:'rzeng0508@gmail.com', password: 'hello', userColor : 'yelloworange'})
+        .end(function(err,res) {
+            expect(res.header.location).to.include('profile');
+            done(err);
+        })
+    });
+
     it('Successful logins redirect to /profile', function(done) {
         request(app)
         .post('/login/')
@@ -31,5 +49,32 @@ describe('Passport authentication', function() {
         });
     });
 
+    it('Successful logouts redirect to homepage', function(done) {
+        request(app)
+        .post('/login/')
+        .type('form')
+        .send({email: 'rzeng0508@gmail.com' , password: 'hello'})
+        .end(function(err,res) {
+            request(app)
+            .get('/logout/')
+            .end(function(err2,res2){
+                expect(res2.header.location).to.equal('/');
+                done(err2);
+            });
+        });
+    });
 
+
+    afterEach(function(done) {
+        request(app)
+        .get('/logout/')
+        .end(function(err,res) {
+            done(err);
+        });
+    });
+
+    after(function(done) {
+        User.collection.drop();
+        done();
+    });
 });
