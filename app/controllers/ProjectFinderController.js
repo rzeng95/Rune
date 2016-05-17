6158155cf8815bc249a3d41036240b6883585f57
Alex Wang <alexlw92@yahoo.com>
2016-05-16 23:33:51 -0700

    jumping off point for project finder. also updated base.jade

--- app/controllers/ProjectFinderController.js
+++ app/controllers/ProjectFinderController.js
@@ -1,13 +1,23 @@
 var Helper = require('../models/helpers.js');
+var Project = require('../models/project.js');
 
 module.exports = function(app, passport) {
     app.get('/projectfinder', Helper.isLoggedIn, function(req, res) {
-        res.render('projectfinder.jade', {
-            // These are navbar variables
-            loggedIn : req.isAuthenticated(),
-            projList : req.user.local.projects,
-            firstname : req.user.local.firstname
+    	Project.find({}, function(err,projs) {
+            if (err) {
+                throw err;
+            } else {
+            	console.log(projs);
+            	res.render('projectfinder.jade', {
+		            // These are navbar variables
+		            loggedIn : req.isAuthenticated(),
+		            projList : req.user.local.projects,
+		            firstname : req.user.local.firstname,
+		            projects : projs
+		        });
+		    }
         });
+        
     });
 
 };
