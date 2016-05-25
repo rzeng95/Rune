8fd3c40fe859faab6186957fff784ebf0853a590
Brandon Ly <brandonly@live.com>
2016-05-25 15:23:25 -0700

    Even more tests for github auth

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,6 @@ module.exports = {
     'githubAuth' : {
         'clientID' : 'fb79527a871e5ba8f0f7',
         'clientSecret' : 'a82aa7f700c3f1022aefa81abdf77cf590593098',
-        'callbackURL' : '/auth/github/callback/'
+        'callbackURL' : 'http://rune130.herokuapp.com/auth/github/callback/'
     }
-
 }
