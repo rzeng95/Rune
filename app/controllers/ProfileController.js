5e2c06fd1f81268f5ba54c23ee805dc44c06db08
Roland <roland.zeng@gmail.com>
2016-05-26 22:54:40 -0700

    user deletion endpoint works again
    Still needs an "are you sure" button before deleting users.

--- app/controllers/ProfileController.js
+++ app/controllers/ProfileController.js
@@ -269,7 +269,7 @@ module.exports = function(app, passport) {
         });
     }); //End of app.get('/users')
 
-    app.post('/u/:userid/delete', Helper.isLoggedIn, Helper.doesUserExist, function(req, res) {
+    app.post('/u/:userid/edit/delete', Helper.isLoggedIn, Helper.doesUserExist, function(req, res) {
         async.waterfall([
             function getUser(callback) {
                 User.findOne({'local.userid': req.params.userid}, function(err, foundUser) {
